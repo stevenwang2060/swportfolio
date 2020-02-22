69b3656f3fe41f6291732f6b43dbd2d8f7e3e9db
Steven Wang <stevenwang2060@gmail.com>
2020-02-22 08:29:15 -0800

    Version 1.3

--- src/components/Project/ProjectCard.js
+++ src/components/Project/ProjectCard.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components'
 import {
-    Card, CardImg, CardText, CardBody,
+    Card, CardText, CardBody,
     CardTitle, CardSubtitle
   } from 'reactstrap';
 
